e3d8b352de656bfcea2f9c12877d752d6c926dbc
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-17 14:46:07 +0200

    added mailList array & check if access is allowed

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,4 @@
+/* Gioco dei dadi */
 let playerNumber = Math.floor((Math.random() * 6) + 1);
 let computerNumber = Math.floor((Math.random() * 6) + 1);
 console.log("Il numero del tuo dado è " + playerNumber);
@@ -9,4 +10,26 @@ if (playerNumber == computerNumber) {
     console.log("Hai vinto!");
 } else {
     console.log("Hai perso!");
+}
+
+/* Mail */
+const mailList = [
+    "cloud.strife@gmail.com",
+    "tifa.lockhart@outlook.com",
+    "aerith.gainsborough@gmail.com"
+]
+
+let access = false;
+const userMail = prompt("Inserisci la tua mail");
+
+for (let i = 0; i < mailList.length; i++) {
+    if (userMail === mailList[i]) {
+        access = true;
+    }
+}
+
+if (access === true) {
+    console.log("Accesso consentito");
+} else {
+    console.log("Accesso negato");
 }
