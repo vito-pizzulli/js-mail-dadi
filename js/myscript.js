2d14eeb9f9bcc76624d617ac77a00fdd9a085d05
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-17 14:15:17 +0200

    added random dice n 1 to 6 for player and computer

---
+++ js/myscript.js
@@ -0,0 +1,4 @@
+let playerNumber = Math.floor((Math.random() * 6) + 1);
+let computerNumber = Math.floor((Math.random() * 6) + 1);
+console.log("Il numero del tuo dado è " + playerNumber);
+console.log("Il numero del dado dell'avversario è " + computerNumber);
