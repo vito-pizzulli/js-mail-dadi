779518e5f3de186fe3990f641d52d3aaea5fe814
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-17 14:21:30 +0200

    added if, else if, else to check the winner

--- js/myscript.js
+++ js/myscript.js
@@ -2,3 +2,11 @@ let playerNumber = Math.floor((Math.random() * 6) + 1);
 let computerNumber = Math.floor((Math.random() * 6) + 1);
 console.log("Il numero del tuo dado è " + playerNumber);
 console.log("Il numero del dado dell'avversario è " + computerNumber);
+
+if (playerNumber == computerNumber) {
+    console.log("E' un pareggio!");
+} else if (playerNumber > computerNumber) {
+    console.log("Hai vinto!");
+} else {
+    console.log("Hai perso!");
+}
